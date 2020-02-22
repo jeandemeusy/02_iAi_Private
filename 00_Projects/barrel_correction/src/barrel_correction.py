5b59e9423ea0aa6a0e39d63556b6f4fe2056d886
Jean Demeusy <jean.demeusy@heig-vd.ch>
2020-02-22 01:21:59 +0100

    Simplified code

--- 00_Projects/barrel_correction/src/barrel_correction.py
+++ 00_Projects/barrel_correction/src/barrel_correction.py
@@ -1,14 +1,7 @@
 import numpy as np
 import cv2 as cv
-import scipy.io
 
 from image import Image
-
-# Functions
-def load_mat_file(path,key='image'):
-    mat = scipy.io.loadmat(path)
-    return mat[key]
-
 
 # Variables
 k1 = -0.00925
@@ -26,6 +19,6 @@ max_r_d2 = np.amax(r_d2)
 x_u = np.float32(np.multiply(1+r_d2*k1/max_r_d2,x) + w/2)
 y_u = np.float32(np.multiply(1+r_d2*k1/max_r_d2,y) + h/2)
 
-im_interp = cv.remap(im,x_u,y_u,cv.INTER_NEAREST)
+im_interp = cv.remap(im.data,x_u,y_u,cv.INTER_NEAREST)
 
 C = Image(data=np.dstack((im_interp,im.data,im_interp))).show()
--- 00_Projects/barrel_correction/src/file.py
+++ 00_Projects/barrel_correction/src/file.py
@@ -4,16 +4,3 @@ class File():
     def __init__(self, path):
         assert(isinstance(path,str))
         self.path = path
-
-    def to_DXF(self, foldername = 'DXFs'):
-        #print(os.split(self.path))
-
-        name = self.path.replace('\\','/').split('/')
-        name[-1] = name[-1].replace('png', 'dxf')
-        name[-2] = foldername
-
-        folder = '/'.join(name[:-1])
-        if not path.isdir(folder):
-            mkdir(folder)
-
-        return DXF_File('/'.join(name))
